8af2282a6f7b932f48a02c762d0cfd0c8b978963
Prem Kumar <prem68265@gmail.com>
2024-11-13 14:30:16 +0530

    invontary update

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,56 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Spinner from 'react-bootstrap/Spinner';
 import Accordion from 'react-bootstrap/Accordion';
 import Table from 'react-bootstrap/Table';
-
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 function App() {
   const [url, setUrl] = useState('');
   const [num, setNum] = useState(0);
   const [productUrl, setProductUrl] = useState([]);
   const [file, setFile] = useState('');
+  const [invfile, setInvFile] = useState('');
   const [loading, setLoading] = useState(false);
   const [upc,setUpc]=useState([{}]);
+  const [links, setLinks] = useState([]);
+  const [invProduct,setInvProduct]=useState([{}]);
+
 
   useEffect(() => {
+    getinvurl();
+    getinvproducts();
     getproductslink();
-  }, [])
+    
+  }, []);
+
+  const getinvproducts=async()=>{
+    try {
+      console.log("get links called")
+      let result = await fetch('https://brand-b.onrender.com/getinvproduct', {
+        method: "GET",
+        headers: { 'Content-Type': 'application/json' }
+      })
+      result = await result.json();
+      setInvProduct(result);
+      console.log(result)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  const getinvurl = async () => {
+    try {
+      console.log("get links called")
+      let result = await fetch('https://brand-b.onrender.com/getinvurl', {
+        method: "GET",
+        headers: { 'Content-Type': 'application/json' }
+      })
+      result = await result.json();
+      setLinks(result.links[0].url);
+      // setNoOfTotalPr(result.notp)
+    } catch (err) {
+      console.log(err)
+    }
+  };
 
   const fetchbrand = async () => {
     if (num > 0) {
@@ -131,6 +168,71 @@ function App() {
     setUpc(data.upc);
     console.log(data)
   }
+
+  // --------upload file for inventory update----
+  const setInventoryfile = (e) => {
+    setInvFile(e.target.files[0]);
+  };
+
+  const uploadinventoryfile = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file',invfile);
+
+    try {
+      const response = await axios.post('https://brand-b.onrender.com/uploadinvfile', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      alert(response.data.msg);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      alert('Failed to upload file');
+    }
+  };
+
+  // ----auto fetch data call----------
+
+  const autofetchData = async (link) => {
+    try {
+      console.log("Fetching data for:", link);
+      let result = await fetch('https://brand-b.onrender.com/autofetchdata', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ link: link })
+      });
+      result = await result.json(); 
+      return result
+    } catch (err) {
+      console.log("Error in autofetchData:", err);
+      return false; // Return false in case of error to prevent further execution
+    }
+  };
+  
+  const autofetch = async () => {
+    console.log("Starting autofetch...");
+    let index = 0; // Starting index
+  
+    while (index < 5) {
+      try {
+        const result = await autofetchData(links[index]); // Wait for autofetchData to complete
+        
+        console.log("Index:", index);
+        console.log("Result:", result);
+        if (result === true) {
+          index++; 
+          // getnumberofupdatedpr(); 
+        } else {
+          console.log("No update required for this link or an error occurred.");
+           index=index
+        }
+      } catch (err) {
+        console.log("Error in autofetch:", err);
+        break; 
+      }
+    }
+  };
   return (
     <div style={{ opacity: loading ? 0.5 : 1, color: loading ? 'black' : null }}>
       {loading && ( // Show spinner while loading is true
@@ -159,9 +261,6 @@ function App() {
         <button onClick={downloadFinalSheet}>Download Comparison Sheet</button>
 
       </div>
-
-
-
       <Accordion className='mt-4'>
         <Accordion.Item eventKey="0">
           <Accordion.Header>Number of Products fetched : {productUrl.length}</Accordion.Header>
@@ -187,7 +286,7 @@ function App() {
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
-        <Accordion.Header>Price Increased: {upc.length}</Accordion.Header>
+        <Accordion.Header>TOtal UPCs list: {upc.length}</Accordion.Header>
         <Accordion.Body>
         <Table striped bordered hover>
             <thead>
@@ -219,11 +318,95 @@ function App() {
           </Table>
         </Accordion.Body>
       </Accordion.Item>
-
-
-
-
       </Accordion>
+      <hr />
+      <div>
+        <h2>Inventory Updation</h2>
+        <form onSubmit={uploadinventoryfile}>
+          <input type="file" onChange={setInventoryfile} accept=".xlsx, .xls" />
+          <button type="submit">Upload</button>
+        </form>
+        <Button variant="secondary" className='me-4' onClick={autofetch}>
+        Start Auto Fetch
+      </Button>
+      </div>
+
+      <Accordion className='mt-4'>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Number of Product's URL to be fetched : {links?links.length:0}</Accordion.Header>
+          <Accordion.Body>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>S.No</th>
+                  <th>Inventory Products' url</th>
+                </tr>
+              </thead>
+              { links.length > 0 && links.map((l, i) => (
+                <tbody>
+                  <tr key={i}>
+                    <td style={{ padding: '0 !important' }}>
+                      {i + 1}
+                    </td>
+                    <a style={{background:'white !important'}} href={l}>{l}</a>
+                  </tr>
+                </tbody>
+              ))}
+            </Table>
+          </Accordion.Body>
+        </Accordion.Item>
+
+        {/* -------inventory updated price and quantity---- */}
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Total Updated Products detail : {invProduct>1?invProduct.length:0}</Accordion.Header>
+          <Accordion.Body>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                <th>No</th>
+                <th>Image</th>
+                <th>UPC</th>
+                <th>New Price</th>
+                <th>Offer Price</th>
+                <th>Old Price</th>
+                <th>Color & Size</th>
+                <th>Quantity</th>
+                <th>Available</th>
+                <th>Product URL</th>
+                </tr>
+              </thead>
+              { invProduct.length > 0 && invProduct.map((detailArray, i) => (
+                <tbody>
+
+                <tr key={i}>
+                  <td style={{ padding: '0 !important' }}>
+                    {i + 1}
+                  </td>
+                  <td style={{ padding: '0 !important' }}>
+                    <img src={detailArray.imgurl} alt="" height='40px' />
+                  </td>
+                  <td>{detailArray.upc}</td>
+                  <td>{detailArray.newPrice}</td>
+                  {detailArray.onsale==='false'  && detailArray.offer !==null ?<td style={{ color: "#0b6bff", fontWeight: 'bold' }}>{detailArray.newPrice  && (detailArray.newPrice - (detailArray.newPrice * Number(detailArray.offer) / 100)).toFixed(2)} <br /> <span style={{color:'black', fontWeight:'initial'}}>{detailArray.offerend}</span></td>
+                : <td>NA</td> }
+                  <td style={{ color: Number(detailArray.oldPrice) !== Number(detailArray.newPrice) ? 'red' : 'black' }}>
+                    {detailArray.oldPrice && detailArray.oldPrice.toFixed(2)}
+                  </td>
+                  <td>{detailArray.clrsize}</td>
+                  <td style={{ color: (detailArray.quantity < 8 && detailArray.available === 'T') || (detailArray.quantity > 8 && detailArray.available === 'F') ? 'red' : 'black' }}>
+                  {detailArray.quantity}
+                  </td>
+                  <td>{detailArray.available}</td>
+                  <td> <a href={detailArray.url} target='_blank'>{detailArray.url}</a> </td>
+                </tr>
+
+              </tbody>
+              ))}
+            </Table>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <hr />
     </div>
 
 
