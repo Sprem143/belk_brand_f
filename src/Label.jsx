bc843f6b4df453d0d5e51443007fcfb5f1dac638
Prem Kumar <prem68265@gmail.com>
2025-03-04 17:11:11 +0530

    Add files via upload

---
+++ src/Label.jsx
@@ -0,0 +1,14 @@
+import { Button } from "react-bootstrap"
+
+export default function Label(){
+
+    return(
+        <>
+        <h3 className="text-center mb-4">
+            Label Generation
+        </h3>
+    <input type="file" />
+    <Button>Upload Sheet</Button>
+        </>
+    )
+}
