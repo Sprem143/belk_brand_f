c4e9440be7f13b6f66f03475d614007596a74715
Prem Kumar <prem68265@gmail.com>
2025-03-11 12:48:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -733,9 +733,9 @@ function App() {
         <div className="container-fluid border p-4 mt-4 mb-1">
           <div className="row">
             <div className="col-md-4 border">
-              <h5 className="mb-2">Download Required Format </h5>
+              <h5 className="mb-2 mt-2">Download Required Format </h5>
               <button onClick={downloadformat}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="55" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
                   <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
                   <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" />
                 </svg>
@@ -743,14 +743,14 @@ function App() {
             </div>
             <div className="col-md-4 border p-2">
               <h5 className="mb-3">Choose Excel Sheet</h5>
-              <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="me-4 bi bi-upload" viewBox="0 0 16 16">
-                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
-                <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z" />
-              </svg> <input type="file" accept=".xlsx, .xls" onChange={setInventoryfile} />
+             <input type="file" accept=".xlsx, .xls" onChange={setInventoryfile} />
             </div>
             <div className="col-md-4 border p-2">
               <h5 className="mb-3">Upload </h5>
-              <button onClick={uploadinventoryfile} >Upload</button>
+              <button onClick={uploadinventoryfile} >  <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="me-4 bi bi-upload" viewBox="0 0 16 16">
+                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
+                <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z" />
+              </svg>  </button>
             </div>
 
           </div>
